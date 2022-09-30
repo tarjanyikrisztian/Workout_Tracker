3ab290b18353266d4917893ecacb1fdd4d453634
Tarjányi Krisztián <krisztian0218@gmail.com>
2022-09-30 11:28:59 +0200

    2

--- client/src/models/Login.jsx
+++ client/src/models/Login.jsx
@@ -4,25 +4,38 @@ import '../css/LoginReg.css'
 
 export const Login = () => {
     return (
-        <div class="container">
-            <div class="content">
-                    <h2>Log In</h2>
-                <form action="">
-                    <span class="input-item">
-                        <i class="fa-solid fa-envelope"></i>
-                        <input class="form-input" id="email" type="email" placeholder="Email" required />
-                    </span>
-                    <span class="input-item">
-                        <i class="fa fa-key"></i>
-                        <input class="form-input" type="password" placeholder="Password" id="password" name="password" required />
-                        <i class="fa fa-eye" aria-hidden="true" type="button" id="eye"></i>
-                        
-                    </span>
-                    <a href="#" className='forgotPassword'>Forgot Password</a>
-                    <button class="btn">Log In</button>
-                </form>
+        <div className="wrap">
+            <div className="containerBorder">
+                </div>
+            <div class="container">
+            
+                <div class="content">
                 
+                    <h2>Log In</h2>
+                    <form action="">
+                        <span class="input-item">
+                            <i class="fa-solid fa-envelope"></i>
+                            <input class="form-input" id="email" type="email" placeholder="Email" required />
+                        </span>
+                        <span class="input-item">
+                            <i class="fa fa-key"></i>
+                            <input class="form-input" type="password" placeholder="Password" id="password" name="password" required />
+                            <i class="fa fa-eye" aria-hidden="true" type="button" id="eye"></i>
+
+                        </span>
+                        <a href="#" className='forgotPassword'>Forgot Password</a>
+                        <button class="btn">Log In</button>
+                    </form>
+                    <div className="or">
+                        or
+                    </div>
+                    <div className="btn googleBtn">
+
+                    </div>
+
+                </div>
             </div>
+            
         </div>
     )
 }
